3b29895021810efcaf8ce091f8e3fc3f0aebd68a
peshwar9 <par.esh1937@gmail.com>
2021-04-14 17:22:36 +0530

    chapter6 added

---
+++ chapter6/ezytutors/tutor-db/src/iter5/handlers/general.rs
@@ -0,0 +1,13 @@
+use crate::errors::EzyTutorError;
+use crate::state::AppState;
+use actix_web::{web, HttpResponse};
+
+pub async fn health_check_handler(
+    app_state: web::Data<AppState>,
+) -> Result<HttpResponse, EzyTutorError> {
+    let health_check_response = &app_state.health_check_response;
+    let mut visit_count = app_state.visit_count.lock().unwrap();
+    let response = format!("{} {} times", health_check_response, visit_count);
+    *visit_count += 1;
+    Ok(HttpResponse::Ok().json(&response))
+}
